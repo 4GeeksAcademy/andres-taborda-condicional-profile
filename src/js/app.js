92abca3ffb9f7c84e1505f256f283231c72d5433
Andres Taborda <84872685+AndresTabor@users.noreply.github.com>
2024-10-02 18:22:20 +0000

    solved exercise

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name ? variables.name : "Julia"} ${
+    variables.lastName ? variables.lastName : ""
+  } </h1>
+          <h2>${variables.role ? variables.role : "Web Developer"}</h2>
+          <h3>${variables.city ? variables.city : "Miami"}, ${
+    variables.country ? variables.country : "USA"
+  }</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="${
+              variables.twitter
+                ? variables.twitter
+                : "https://twitter.com/4geeksacademy"
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github
+                ? variables.github
+                : "https://github.com/4geeksacademy"
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+                ? variables.linkedin
+                : "https://linkedin.com/school/4geeksacademy"
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+                ? variables.instagram
+                : "https://instagram.com/4geeksacademy"
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
